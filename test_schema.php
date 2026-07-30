9fb04f2e6b7293c3c95884f330194079faa08329
Faheem <faheemahmedsoomro6@gmail.com>
2026-07-30 10:44:36 +0500

    chore: remove temporary test files

--- test_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-$db = Database::getInstance()->getConnection();
-foreach (['hods', 'committees', 'coordinators', 'supervisors'] as $table) {
-    echo "TABLE $table:\n";
-    $stmt = $db->query("DESCRIBE `$table`");
-    print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-}
