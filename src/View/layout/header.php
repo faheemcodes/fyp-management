e3f998628a266ba0e84ce8c5f3c9cc5376c0fd5c
Faheem <faheemahmedsoomro6@gmail.com>
2026-08-29 10:18:49 +0500

    Fix mobile browser address bar sidebar viewport clipping with 100dvh and safe area insets

--- src/View/layout/header.php
+++ src/View/layout/header.php
@@ -18,8 +18,8 @@
     }
     ?>
     <link rel="icon" href="<?php echo $basePath; ?>/images/logo.png" type="image/png">
-    <link href="<?php echo $basePath; ?>/css/style.css?v=1.2.0" rel="stylesheet">
-    <link href="<?php echo $basePath; ?>/css/admin-theme.css?v=1.2.0" rel="stylesheet">
+    <link href="<?php echo $basePath; ?>/css/style.css?v=1.2.1" rel="stylesheet">
+    <link href="<?php echo $basePath; ?>/css/admin-theme.css?v=1.2.1" rel="stylesheet">
     <script>
         if (localStorage.getItem('theme') === 'dark') {
             document.documentElement.classList.add('dark-theme');
